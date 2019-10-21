6776744dce902c6d45c2ebeda1e04571481ab9ce
David Banks <amoebae@gmail.com>
2019-10-21 15:18:17 +0100

    refactor transformer

--- src/transform.ts
+++ src/transform.ts
@@ -1,9 +1,54 @@
 import {
-    INTERFACES_FILE_VERSION, AggregatedForm, ModelInsert, IdGenerator, ModelInsertSpec
+    INTERFACES_FILE_VERSION, AggregatedForm, ModelInsert, IdGenerator,
+    ModelInsertSpec, Person
 } from '@/interfaces';
 import { getIsoDate } from './date';
 
 console.log(INTERFACES_FILE_VERSION);
+
+function transformPerson(p: Person, idGenerator: IdGenerator, flightId: string): ModelInsertSpec[] {
+    const thisPersonId = idGenerator();
+    const result: ModelInsertSpec[] = [];
+
+    const personSpec: ModelInsertSpec = {
+        cypherId: 'createPerson',
+        queryParameters: {
+            name: p.name,
+            id: thisPersonId
+        }
+    };
+
+    result.push(personSpec);
+
+    // Add all the person's aliases
+
+    p.aliases.forEach(a => {
+        const aliasId = idGenerator();
+        const alias = {
+            cypherId: 'createAlias',
+            queryParameters: {
+                alias: a.name,
+                id: aliasId
+            }
+        };
+        result.push(alias)
+
+        const aliasContextId = idGenerator();
+        const aliasContext = {
+            cypherId: 'createAliasContext',
+            queryParameters: {
+                id: aliasContextId,
+                personId: thisPersonId,
+                aliasId: aliasId,
+                flightId: flightId
+            }
+        };
+
+        result.push(aliasContext);
+    });
+
+    return result;
+}
 
 export function toNeo4jParameters(form: AggregatedForm, idGenerator: IdGenerator): ModelInsert {
     // XXX: it's not testable to call all of these methods here
@@ -22,46 +67,9 @@ export function toNeo4jParameters(form: AggregatedForm, idGenerator: IdGenerator
         }
     ];
 
+    // Extend array with the stuff that's person-related
     form.persons.forEach(p => {
-        const thisPersonId = idGenerator();
-
-
-        const personSpec: ModelInsertSpec = {
-            cypherId: 'createPerson',
-            queryParameters: {
-                name: p.name,
-                id: thisPersonId
-            }
-        };
-
-        base.push(personSpec);
-
-        // Add all the person's aliases
-
-        p.aliases.forEach(a => {
-            const aliasId = idGenerator();
-            const alias = {
-                cypherId: 'createAlias',
-                queryParameters: {
-                    alias: a.name,
-                    id: aliasId
-                }
-            };
-            base.push(alias)
-
-            const aliasContextId = idGenerator();
-            const aliasContext = {
-                cypherId: 'createAliasContext',
-                queryParameters: {
-                    id: aliasContextId,
-                    personId: thisPersonId,
-                    aliasId: aliasId,
-                    flightId: flightId
-                }
-            };
-
-            base.push(aliasContext);
-        });
+        base.push(...transformPerson(p, idGenerator, flightId))
     });
 
     console.log("transformed to %o", base);
