67f132f99591792c7d81e31fb9750422a6bfc557
David Banks <amoebae@gmail.com>
2019-10-21 12:44:17 +0100

    embedding table in popover

--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -2,6 +2,11 @@
 <div>
   <h1>ws</h1>
 
+
+      <el-dialog title="Help"
+                 :visible.sync="dialogVisible"
+                 width="30%">
+
   <el-table :data="tableData">
     <el-table-column label="Name">
       <template slot-scope="scope">
@@ -18,6 +23,17 @@
   </el-table>
 
   <el-button type="primary" v-on:click="addRow">Add</el-button>
+
+
+        
+        <span slot="footer" class="dialog-footer">
+          <el-button v-on:click="dialogVisible = false">OK</el-button>
+        </span>
+      </el-dialog>
+
+
+
+  <el-button v-on:click="pop">Pop</el-button>
 </div>
 </template>
 
