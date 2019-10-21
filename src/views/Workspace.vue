9e492ecebe6be8cbb7d0947a7e50b435a8aef750
David Banks <amoebae@gmail.com>
2019-10-21 16:43:07 +0100

    requesting sheet

--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -2,21 +2,26 @@
 <div>
   <h1>ws</h1>
 
+  <textarea cols="80" rows="8">{{stuff}}</textarea>
+
+
 </div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
+import Vue from 'vue';
 import axios from 'axios';
 
 export default Vue.extend({
     data() {
         return {
+            stuff: null as any
         };
     },
     created() {
         axios.get("/sensitive/tillet_converted.json").then(r => {
             console.log("response is %o", r);
+            this.stuff = r.data;
         }).catch(e => {
             console.log("error is %o", e);
         });
