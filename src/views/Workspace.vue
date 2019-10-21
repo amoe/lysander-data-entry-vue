c3b32144154591b2438ef230fab1e813e70a12f6
David Banks <amoebae@gmail.com>
2019-10-21 12:41:20 +0100

    demonstrate list management

--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -1,18 +1,23 @@
 <template>
 <div>
   <h1>ws</h1>
-  
-  <el-dialog title="Alais"
-             :visible.sync="dialogVisible"
-             width="30%">
-    <p>This is content this is content</p>
 
-    <span slot="footer" class="dialog-footer">
-      <el-button v-on:click="dialogVisible = false">OK</el-button>
-    </span>
-  </el-dialog>
+  <el-table :data="tableData">
+    <el-table-column label="Name">
+      <template slot-scope="scope">
+        <el-input v-model="scope.row.name"/>
+      </template>
+    </el-table-column>
+    <el-table-column label="Operations">
+      <template slot-scope="scope">
+        <el-button size="mini"
+                   type="danger"
+                   v-on:click="handleDelete(scope.$index)">Delete</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 
-  <el-button v-on:click="pop">Pop</el-button>
+  <el-button type="primary" v-on:click="addRow">Add</el-button>
 </div>
 </template>
 
@@ -22,12 +27,21 @@ import Vue from 'vue';
 export default Vue.extend({
     data() {
         return {
-            dialogVisible: false
+            dialogVisible: false,
+            tableData: [
+                {name: 'fry', age: 29}
+            ]
         };
     },
     methods: {
         pop() {
             this.dialogVisible = true;
+        },
+        handleDelete(index: number) {
+            this.tableData.splice(index, 1);
+        },
+        addRow() {
+            this.tableData.push({name: "", age: 0});
         }
     }
 });
