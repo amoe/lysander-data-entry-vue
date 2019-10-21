f2d8849132fc8048060fe70ad74179a628b2597e
David Banks <amoebae@gmail.com>
2019-10-21 17:40:35 +0100

    spreadsheet view

--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -2,9 +2,56 @@
 <div>
   <h1>ws</h1>
   
-  <el-table :data="tilletData" :span-method="mySpanMethod">
+  
+  <el-table :data="tilletData" cell-class-name="tillet-cell">
     <el-table-column prop="record_id" label="Record ID"/>
     <el-table-column prop="date" label="Date"/>
+
+
+    <el-table-column label="Landing zone">
+      <template slot-scope="scope">
+        <el-table :data="wrapPlainStrings(scope.row.landing_zone)" :show-header="false"
+                  cell-class-name="tillet-cell">
+          <el-table-column prop="content"/>
+        </el-table>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="References">
+      <template slot-scope="scope">
+        <el-table :data="wrapPlainStrings(scope.row.references)" :show-header="false"
+                  cell-class-name="tillet-cell">
+          <el-table-column prop="content"/>
+        </el-table>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Passengers out">
+      <template slot-scope="scope">
+        <el-table :data="wrapPlainStrings(scope.row.passengers_out)" :show-header="false"
+                  cell-class-name="tillet-cell">
+          <el-table-column prop="content"/>
+        </el-table>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Operation">
+      <template slot-scope="scope">
+        <el-table :data="wrapPlainStrings(scope.row.operation)" :show-header="false"
+                  cell-class-name="tillet-cell">
+          <el-table-column prop="content"/>
+        </el-table>
+      </template>
+    </el-table-column>
+
+    <el-table-column label="Squadron">
+      <template slot-scope="scope">
+        <el-table :data="wrapPlainStrings(scope.row.squadron)" :show-header="false"
+                  cell-class-name="tillet-cell">
+          <el-table-column prop="content"/>
+        </el-table>
+      </template>
+    </el-table-column>
   </el-table>
 </div>
 </template>
@@ -22,17 +69,26 @@ export default Vue.extend({
     },
     created() {
         axios.get("/sensitive/tillet_converted.json").then(r => {
-            this.tilletData = r.data;
+            this.tilletData = r.data.slice(0, 5);
         }).catch(e => {
             console.log("error is %o", e);
         });
     },
     methods: {
-        mySpanMethod(foo: any) {
-            console.log("span method was called");
+        wrapPlainStrings(aliases: string[]): object {
+            return aliases.map(function(x) {
+                return {content: x};
+            });
         }
     },
     computed: {
     }
 });
 </script>
+
+<style lang="less">
+.tillet-cell {
+    font-family: Arial, sans-serif;
+    font-size: 8pt;
+}
+</style>
