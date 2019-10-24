b509a54a617cd73a7873e66d385a7eb08035f988
David Banks <amoebae@gmail.com>
2019-10-24 18:36:42 +0100

    tab interface demo

--- src/views/Workspace.vue
+++ src/views/Workspace.vue
@@ -1,8 +1,13 @@
 <template>
 <div>
-<h1>ws</h1>
+  <h1>ws</h1>
 
-
+  <el-tabs type="border-card" v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="User" name="first">User</el-tab-pane>
+    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  </el-tabs>
 </div>
 </template>
 
@@ -15,6 +20,11 @@ import mc from '@/mutation-constants';
 
 export default Vue.extend({
     components: {SearchSelect},
+    data() {
+        return {
+            activeName: 'first'
+        };
+    },
     methods: {
     },
     computed: {
