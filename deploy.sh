645bfdf38a48f68bc856936188999a33ec9af64c
David Banks <amoebae@gmail.com>
2019-12-24 17:03:04 +0000

    change deploy path

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -eu
 
 rm -rf dist
 npm run build
-rsync --bwlimit=128k -aPv --delete dist/ visarend.solasistim.net:/srv/http/lysander_old/
+rsync --bwlimit=128k -aPv --delete dist/ visarend.solasistim.net:/srv/http/lysander-old/
